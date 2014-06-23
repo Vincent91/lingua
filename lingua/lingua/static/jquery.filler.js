69f4a56c230373aefeecdac85e06349b9c11b9aa
Vincent91 <kononov.vincent91@gmail.com>
2014-06-23 22:14:57 +0400

    added popup to see full description of nouns

--- lingua/lingua/static/jquery.filler.js
+++ lingua/lingua/static/jquery.filler.js
@@ -1,7 +1,14 @@
 $(document).ready(function () {
 
-	$("#content .alt").live("click", function() {
-		alert($(this).text());
+	$("#content").on("click", "td", function() {
+		var aes = $(this).attr("attributeES");
+		var ais = $(this).attr("attributeIS");
+		var aisa = $(this).attr("attributeISA");
+		var aip = $(this).attr("attributeIP");
+		var aipa = $(this).attr("attributeIPA");
+		$("#myModal").html("<h1>" + aes + "</h1><h2>" + aisa + " " + ais + "</h2><h2>" + aipa + " " + aip + 
+			"</h2><a class='close-reveal-modal'>&#215;</a>");
+		$("#revelator").click();
 	});
 
-}
+});
